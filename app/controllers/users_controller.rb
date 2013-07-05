9f4fe926034b4c728cc104807cc485274fe592af
JohnyDays <joao.l.d.a@hotmail.com>
2013-07-05 12:28:43 +0100

    Added Follower Logic to database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,6 @@ def login
         cookies[:user_token] = @user.token
       end
     end
-    respond_to do |format|
-      format.html{redirect_to "/users/" << @user.id << "/posts"}
-    end
+    redirect_to user_posts_path(@user)
   end
 end
