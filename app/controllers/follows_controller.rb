92cdc69ae665069b4083c77c373c96689f4da963
JohnyDays <joao.l.d.a@hotmail.com>
2013-07-17 15:40:28 +0100

    Minor interface improvements

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,29 +1,38 @@
 class FollowsController < ApplicationController
-before_filter :get_user
-def get_user
-@user = User.find(params[:following_id])
-end
-def create
-  if isUserFollowing(@user)
-  flash[:error] = "You are already following " + @user.username + "."
-  redirect_to user_posts_path(@user)
-else
-  @follow = currentuser.follows.new
-  @follow.following_id = params[:following_id]
-  @follow.save
-  flash[:success] = "You have followed " + @user.username + "."
-  redirect_to user_posts_path(@user)
-end
-end
-def destroy
- if isUserFollowing(@user)
-  @follow = currentuser.follows.find_by_following_id(@user.id)
-  @follow.destroy
-  flash[:success] = "You are no longer following " + @user.username + "."
-  redirect_to user_posts_path(@user)
-else
-  flash[:success] = "You were not following" + @user.username + "you dummy."
-  redirect_to user_posts_path(@user)
-end
-end
+  before_filter :get_user
+  def get_user
+    if params[:following_id]
+      @user = User.find(params[:following_id])
+    end
+    if params[:id]
+      @user = User.find(params[:id])
+    end
+  end
+  def create
+    if isUserFollowing(@user)
+      flash[:error] = "You are already following " + @user.username + "."
+      redirect_to user_posts_path(@user)
+    else
+      @follow = currentuser.follows.new
+      @follow.following_id = params[:following_id]
+      @follow.save
+      flash[:success] = "You have followed " + @user.username + "."
+      redirect_to user_posts_path(@user)
+    end
+  end
+  def destroy
+    if isUserFollowing(@user)
+      @follow = currentuser.follows.find_by_following_id(@user.id)
+      @follow.destroy
+      flash[:success] = "You are no longer following " + @user.username + "."
+      redirect_to user_posts_path(@user)
+    else
+      flash[:success] = "You were not following" + @user.username + "you dummy."
+      redirect_to user_posts_path(@user)
+    end
+  end
+  def following
+  end
+  def followers
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Instagroom::Application.routes.draw do
   resources :follows
-
+  match '/users/:id',to: redirect("/users/%{id}/posts")
 
   resources :users do
     resources :posts
   end
+
+  match '/users/:id/following', to: "Follows#following", as: 'user_following'
+  match '/users/:id/followers', to: "Follows#followers", as: 'user_followers'
   delete '/follows', to: "Follows#destroy"
-post 'login', to: "Users#login", as:'login'
-match 'logout' , to: 'Users#logout', as: 'logout'
-root :to => "users#index"
+  post 'login', to: "Users#login", as:'login'
+  match 'logout' , to: 'Users#logout', as: 'logout'
+  root :to => "users#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
