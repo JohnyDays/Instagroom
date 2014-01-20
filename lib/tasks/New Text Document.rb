7e65b64c0be5743370f1f0565ebb0205a257c76f
JohnyDays <joao.l.d.a@hotmail.com>
2014-01-20 20:42:22 +0000

    Added image files for instantaneous setup

--- lib/tasks/New Text Document.rb
+++ lib/tasks/New Text Document.rb
@@ -1,19 +1,18 @@
 i = 0
 
 b = 1
-Dir.foreach('C:\\Users\\Joao\\Torrents\\400 Beautiful Cats Pictures and Wallpapers [Up to 2950 Px]\\400 Beautiful Cats Pictures and Wallpapers [Up to 2950 Px]\\Pics and Walls') do |file|
+Dir.foreach('C:\\Users\\Johnydays\\Downloads\\400 Beautiful Cats Pictures and Wallpapers [Up to 2950 Px]\\400 Beautiful Cats Pictures and Wallpapers [Up to 2950 Px]\\Pics and Walls') do |file|
 
   next if file == '.' or file == '..'
 
 puts i
 
  i  += 1
-File.open('C:\\Users\\Joao\\Torrents\\400 Beautiful Cats Pictures and Wallpapers [Up to 2950 Px]\\400 Beautiful Cats Pictures and Wallpapers [Up to 2950 Px]\\Pics and Walls\\' + file, 'rb') do |f|
-  Dir.mkdir('C:\\Users\\Joao\\Google Drive\\Programming\\Ruby\\Instagroom\\public\\uploads\\cat lover ' + b.to_s) unless File.exists?('C:\\Users\\Joao\\Google Drive\\Programming\\Ruby\\Instagroom\\public\\uploads\\cat lover ' + b.to_s)
+File.open('C:\\Users\\Johnydays\\Downloads\\400 Beautiful Cats Pictures and Wallpapers [Up to 2950 Px]\\400 Beautiful Cats Pictures and Wallpapers [Up to 2950 Px]\\Pics and Walls\\' + file, 'rb') do |f|
+  Dir.mkdir('C:\\Users\\Johnydays\\Documents\\GitHub\\Instagroom\\public\\uploads\\cat lover ' + b.to_s) unless File.exists?('C:\\Users\\Johnydays\\Documents\\GitHub\\Instagroom\\public\\uploads\\cat lover ' + b.to_s)
+  Dir.mkdir('C:\\Users\\Johnydays\\Documents\\GitHub\\Instagroom\\public\\uploads\\cat lover ' + b.to_s + '\\' + i.to_s)
 
-  Dir.mkdir('C:\\Users\\Joao\\Google Drive\\Programming\\Ruby\\Instagroom\\public\\uploads\\cat lover ' + b.to_s + '\\' + i.to_s)
-
-  File.open('C:\\Users\\Joao\\Google Drive\\Programming\\Ruby\\Instagroom\\public\\uploads\\cat lover ' + b.to_s + '\\' + i.to_s + '\\image',"wb") do |newfile|
+  File.open('C:\\Users\\Johnydays\\Documents\\GitHub\\Instagroom\\public\\uploads\\cat lover ' + b.to_s + '\\' + i.to_s + '\\image',"wb") do |newfile|
 
     newfile.write(f.read)
 
